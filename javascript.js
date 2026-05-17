3cb0eef635323ed54d84922090a6b9651971e660
yashika tyagi <yashikatyagi2311@gmail.com>
2026-05-17 20:06:09 +0530

    trial added

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+let add = function(a,b){
+    return a+b;
+};
+console.log(add(5,6));
+let square = x => x*x;
+console.log(sqaure(4));
+const greetuser = (name)=> {
+    console.log("hello,${name}!");
+}
+greetuser("alice");//hello, alice!
